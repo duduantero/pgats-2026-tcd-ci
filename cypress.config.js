5cb337859a5a47f6428cd31d52dc2c02e8ed0a40
duduantero <xduduanterox@gmail.com>
2026-06-16 22:15:51 -0300

    chore: ajustar path do cypress-mochawesome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from "cypress";
 import { beforeEach } from "mocha";
 
 export default defineConfig({
-  reporter: "cypress-mochawesome-reporter", // 🛠️ Define o gerador de relatórios
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    html: true,
+    json: false,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+  },
   e2e: {
     baseUrl: "http://localhost:5500",
     supportFile: false,
